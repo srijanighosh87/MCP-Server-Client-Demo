3d30a2ff5abc237dab16092e3a77e99ec6d493b0
Srijani Ghosh <srijanighosh87@gmail.com>
2025-09-09 12:49:25 +0200

    Initial commit for creation of simple MCP server and MCP client

---
+++ Demo.McpClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using MCPSharp;
+
+namespace Demo.McpClient;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the MCP Client demo!");
+
+        var client = new MCPClient(
+                name: "DemoMcpClient",
+                version: "v1.0.0",
+                server: "D:\\Git Repos\\MCP-Server-Client-Demo\\Demo.McpServer\\bin\\Debug\\net9.0\\Demo.McpServer.exe"
+            );
+        var tools = await client.GetToolsAsync();
+        Console.WriteLine($"Available tool count: {tools.Count}");
+        Console.WriteLine("First Available tool:");
+        Console.WriteLine($"- Name: {tools[0].Name}");
+        Console.WriteLine($"- Description: {tools[0].Description}");
+
+        var result = await client.CallToolAsync(name: tools[0].Name, parameters: new Dictionary<string, object?>
+        {
+            { "a", 10 },
+            { "b", 20 }
+        });
+        Console.WriteLine($"Tool call result: {result.Content[0].Text}");
+
+        Console.Read();
+    }
+}
---
+++ Demo.McpServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using MCPSharp;
+using Microsoft.AspNetCore.Http;
+
+namespace Demo.McpServer;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        MCPServer.Register<CalculatorTool>();
+        await MCPServer.StartAsync("Demo.McpServer", "1.0.0");
+        await Task.Delay(Timeout.Infinite);
+    }
+}
+
+public class CalculatorTool
+{
+    [McpTool("addition", "This tool adds two numbers.")]
+    public static int Add([McpParameter(required: true, description: "The first number.")] int a,
+                          [McpParameter(required: true, description: "The second number.")] int b)
+    {
+        return a + b;
+    }
+}
